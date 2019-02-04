9d513270d72cc25bbf133480316c98f3743ea128
Unknown <bubulle088@gmail.com>
2019-02-04 15:06:29 -0500

    Added a aws deploy script

---
+++ PushToAws.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cp lambda_function.py Lambda_package
+cp praw.ini Lambda_package
+cp -r venv/lib/python3.6/site-packages/* Lambda_package
+cd Lambda_package
+zip -r9 ~/package.zip .
+aws lambda update-function-code --function-name invest_bot --zip-file fileb://~/package.zip
+rm -rf ./*
