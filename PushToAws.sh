e31b4189e0a90ca412ef4d75cf8ee7c40dc2724e
Unknown <bubulle088@gmail.com>
2019-02-15 16:22:48 -0500

    made deploy script quieter

--- PushToAws.sh
+++ PushToAws.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cp *.py praw.ini Lambda_package
+cp *.py Lambda_package
 pip install  -r requirements.txt -t Lambda_package -q
 cd Lambda_package
-zip -r9 ~/package.zip .
+zip -uq -r9 ~/package.zip .
 aws lambda update-function-code --function-name invest_bot --zip-file fileb://~/package.zip
 rm -rf ./*
