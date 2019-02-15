e69c0541b508bccff0d63df66508a35873be718f
Unknown <bubulle088@gmail.com>
2019-02-15 11:00:03 -0500

    cleaned up deploy script

--- PushToAws.sh
+++ PushToAws.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-cp lambda_function.py Lambda_package
-cp Reddit.py Lambda_package
-cp Account.py Lambda_package
-cp praw.ini Lambda_package
-cp -r venv/lib/python3.6/site-packages/* Lambda_package
+cp *.py praw.ini Lambda_package
+pip install  -r requirements.txt -t Lambda_package -q
 cd Lambda_package
 zip -r9 ~/package.zip .
 aws lambda update-function-code --function-name invest_bot --zip-file fileb://~/package.zip
